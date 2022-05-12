cf341336bfb1c3e86c6f53e72d241bf773a2e974
asher scott <asher.scott6@gmail.com>
2022-05-12 16:26:00 -0500

    ProgressBar

---
+++ src/ProgressBar.js
@@ -0,0 +1,11 @@
+import "./ProgressBar.css";
+
+function ProgressBar() {
+  return (
+    <div className="progress-bar">
+      <div className="progress"></div>
+    </div>
+  );
+}
+
+export default ProgressBar;
